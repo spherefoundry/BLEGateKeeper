c09a570a849b778b4b1ec5b7d76b32c7c7f4ed10
Michał Tuszyński <srgtuszy@gmail.com>
2013-11-30 13:17:50 +0100

    Added base and main view controllers

---
+++ iOS/BLEGateKeeper/BLEGateKeeper/BLEBaseViewController.h
@@ -0,0 +1,13 @@
+//
+//  BLEBaseViewController.h
+//  BLEGateKeeper
+//
+//  Created by Michal Tuszynski on 30/11/13.
+//  Copyright (c) 2013 Appvetica. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BLEBaseViewController : UIViewController
+
+@end
---
+++ iOS/BLEGateKeeper/BLEGateKeeper/BLEBaseViewController.m
@@ -0,0 +1,38 @@
+//
+//  BLEBaseViewController.m
+//  BLEGateKeeper
+//
+//  Created by Michal Tuszynski on 30/11/13.
+//  Copyright (c) 2013 Appvetica. All rights reserved.
+//
+
+#import "BLEBaseViewController.h"
+
+@interface BLEBaseViewController ()
+
+@end
+
+@implementation BLEBaseViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
