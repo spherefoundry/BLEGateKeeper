aed3fa181ae24983154a356fd90f192a4b92d68d
michalwos <michal.wos@cowom.pl>
2013-11-30 12:55:17 +0100

    removing afnetworking

---
+++ iOS/BLEGateKeeper/BLEGateKeeper/BLERESTConnector.h
@@ -0,0 +1,13 @@
+//
+//  BLERESTConnector.h
+//  BLEGateKeeper
+//
+//  Created by Michał Woś on 30/11/13.
+//  Copyright (c) 2013 Appvetica. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BLERESTConnector : NSObject
+
+@end
---
+++ iOS/BLEGateKeeper/BLEGateKeeper/BLERESTConnector.m
@@ -0,0 +1,14 @@
+//
+//  BLERESTConnector.m
+//  BLEGateKeeper
+//
+//  Created by Michał Woś on 30/11/13.
+//  Copyright (c) 2013 Appvetica. All rights reserved.
+//
+
+#import "BLERESTConnector.h"
+#import "AFNetworking.h"
+
+@implementation BLERESTConnector
+
+@end
