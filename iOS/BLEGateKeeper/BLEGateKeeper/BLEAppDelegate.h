99d550860a76a41d40c2bcf01a6d4d85cba993c4
Michał Tuszyński <srgtuszy@gmail.com>
2013-11-30 12:31:43 +0100

    Added Xcode project

---
+++ iOS/BLEGateKeeper/BLEGateKeeper/BLEAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  BLEAppDelegate.h
+//  BLEGateKeeper
+//
+//  Created by Michal Tuszynski on 30/11/13.
+//  Copyright (c) 2013 Appvetica. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BLEAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ iOS/BLEGateKeeper/BLEGateKeeper/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  BLEGateKeeper
+//
+//  Created by Michal Tuszynski on 30/11/13.
+//  Copyright (c) 2013 Appvetica. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "BLEAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+  @autoreleasepool {
+      return UIApplicationMain(argc, argv, nil, NSStringFromClass([BLEAppDelegate class]));
+  }
+}
